e6dbf5872836ca7aee93c5a0936f1e4f4782c137
tushar gupta <tushargupta10072003@gmail.com>
2026-06-23 23:16:45 +0530

    add validpalindrome 2 leetcode 680 problem

---
+++ STRING/validpalindrome2.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+// core concept to check string is palindrome or not
+bool checkremove(string s,int start,int end){
+    while(start<end){
+        if(s[start]!=s[end]){
+            return false;
+        }
+        start++;
+        end--;
+    }
+    return true;
+}
+
+// check when char from start and end mismatch that after skipping one char from start or skip from left remain the string palindrome
+bool checkpalindromeafterremovingone(string s,int start,int end){
+  while(start<end){  
+   if(s[start]!=s[end]){
+    // jqahan char mismatch wahan ek char skip from left or right and check for palindrome
+       return checkremove(s,start+1,end)||checkremove(s,start,end-1);
+    }
+   start++;
+   end--;
+  }
+  return true;
+//   by default return true as upar sayad if condi check karne par wo exe hi n hui ho
+}
+
+int main(){
+    // leet code -680 givrn a  string check wheater after removing only one character from anywhere does remain string is palindrome or not
+    string s="abcd";
+    int start=0;
+    int end=s.size()-1;
+    bool result=false;
+     result=checkpalindromeafterremovingone(s,start,end);
+    cout<<"string after removing one cahr is:"<<result;
+}
