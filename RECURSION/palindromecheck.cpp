8011f968e8d1e8ef3b2cd2291a0418d6d0e8917d
tushar gupta <tushargupta@tushars-MacBook-Air-2.local>
2026-06-26 09:08:05 +0530

    palindrome check via recursion

---
+++ RECURSION/palindromecheck.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool pali(string &s,int start,int end){
+  if(start>=end){
+    return true;
+  }
+  if(s[start]!=s[end]){
+    return false;
+  }
+  return pali(s,start+1,end-1);
+
+}
+
+int main(){
+    string s="madem";
+    int start=0;
+    int end=s.size()-1;
+    bool palindrome=pali(s,start,end);
+    cout<<palindrome;
+}
