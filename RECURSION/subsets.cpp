e7c2f8b6623d4a09e45f80a5e8b79d3fbb73e47e
tushar gupta <tushargupta@tushars-MacBook-Air-2.local>
2026-07-03 16:03:48 +0530

    finding subsets of a superset

---
+++ RECURSION/subsets.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void subsets(vector<int>&v,int index,int size,vector<int>temp,vector<vector<int>>&ans){
+
+//   when index pint to last element then push temp vector into 2d vector
+if(index==size){
+    ans.push_back(temp);
+    return;
+}
+     
+    // no/ element at that index not include
+    subsets(v,index+1,size,temp,ans);
+    // elemert at that index included
+    temp.push_back(v[index]);
+    subsets(v,index+1,size,temp,ans);
+
+
+
+}
+
+int main(){
+    // space complexity = no of nodes generated in recursive tree i.e->  2^n+1 -1
+    // make two choices at every level of tree either choose and insert index ele in array or not 
+    //if not choosesn and inserted simply increase index+1 and if choosen insert it and index+1
+    // base condition=> jab index given array ke size tak pahunch jaye tab us temp arr ko 2d ans vec m insert kar do
+    vector<int>v={1,2,3};
+    // no of subsets are 2^n  {} 1 2 3 12 13 23
+    vector<int>temp;
+    vector<vector<int>>ans;
+    subsets(v,0,3,temp,ans);
+
+    for(int i=0;i<ans.size();i++){
+        for(int j=0;j<ans[i].size();j++){
+            cout<<ans[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
