36f9276d443818a1978ca3c04b6f92d1c45cdff5
tushar gupta <tushargupta@tushars-MacBook-Air-2.local>
2026-07-06 09:22:17 +0530

    update

--- ARRAYS/oneplusleet66.cpp
+++ ARRAYS/oneplusleet66.cpp
@@ -7,6 +7,7 @@ using namespace std;
 int main() {
 vector<int>v={9,9,9};
 vector<int>ans;
+bool allnine=true;
 
 for(int i=v.size()-1;i>=0;i--){
     // agar last wala ele 9 h to use 0 bana do 
@@ -17,13 +18,16 @@ for(int i=v.size()-1;i>=0;i--){
     // agar nahi h to usme ekadd karo and terminate function two stop  increasing  further digits
     else{
          v[i]++;
+         allnine=false;
          break;
      }
      
     
 }
 // when all digits are no upar wale function se wo sare digits 0 ban jayege then caary k 1 add in begin of ans
+if(allnine){
  v.insert(v.begin(),1);
+} 
 for(auto x:v){
     cout<<x<<" ";
 }
