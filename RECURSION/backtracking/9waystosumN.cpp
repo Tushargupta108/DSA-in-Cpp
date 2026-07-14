a6fb6d7f4bc946039edf08aad75d9acf760b35fb
tushar gupta <tushargupta@tushars-MacBook-Air-2.local>
2026-07-14 23:48:19 +0530

    total ways to find sum equals to N

---
+++ RECURSION/backtracking/9waystosumN.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int countwaystosumN(vector<int>&arr,int target){
+    if(target<0){
+        return 0;
+    }
+    if(target==0){
+        return 1;
+    }
+    
+    // backtracking when target found or bcm negative
+    int ans=0;
+    // helps to add 1 and 0 comes from the brancjes of node in perfectsum ele no repeat then where take and skip work 
+   for(int i=0;i<arr.size();i++){
+    // this loop is used to travese on elements all that particular node from the defined index
+     ans+= countwaystosumN(arr,target-arr[i]);
+   }
+   return ans;
+}
+int main(){
+    // same problem as perfect sum but here elements can repeate themself ex 7 =1+1+1+1+1+1+1
+    int N;
+    cin>>N;
+    vector<int>arr={1,5,6};
+    
+    cout<<"totoalno of ways to sum N  "<<countwaystosumN(arr,N);
+    
+}
---
+++ RECURSION/backtracking/tempCodeRunnerFile.cpp
@@ -0,0 +1,1 @@
+ // marked as visited element
