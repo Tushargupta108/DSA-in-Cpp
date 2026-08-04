db8e1fa918323f331b48ce3c8c42f83a3c900486
tushar gupta <tushargupta10072003@gmail.com>
2026-08-04 22:56:01 +0530

    max avg of subarray of len k

---
+++ ARRAYS/slidingwindow/max_avg_subary1.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int>v={1,12,-5,-6,50,3};
+    int length=4;
+    int start=0;
+    int end=0;
+    int  maxavg=INT_MIN;
+    int sum=0;
+    double avg=0;
+    while(start<=end&&end<v.size()){
+         sum+=v[end];
+        if((end-start+1)==4){
+           maxavg=max(maxavg,sum);
+           sum-=v[start];
+           start++;
+        }
+       end++;
+    }
+    avg=(double)maxavg/4;
+    cout<<avg;
+}
